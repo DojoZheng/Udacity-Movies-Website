ac9527a8fb5a5b44daa197f23ee3a8a3ec633cf0
DojoZheng <zhengdongjia@foxmail.com>
2017-11-25 21:41:53 +0800

    Reconstruct the codes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,42 +1,46 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-						"A story of a boy and his toys that come to life",
-						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-						"http://player.youku.com/embed/XMTE2MDEyNDU2")
-# print(toy_story.storyline)
-# toy_story.show_trailer()
+# initialize the movies data
+movies_data = [
+	# toy_story
+	["Toy Story",
+	"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+	"http://player.youku.com/embed/XMTE2MDEyNDU2"],
 
-avatar = media.Movie("Avatar",
-					"A marine on an alien planet",
-					"http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-					"http://player.youku.com/embed/XMTk4ODUyNDQ4")
+	# avatar
+	["Avatar",
+	"http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+	"http://player.youku.com/embed/XMTk4ODUyNDQ4"],
 
-pianist = media.Movie("The Legend of 1900",
-					"An epic story of a man who could do anything...except be ordinary.",
-					"https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5NjgyMjEyMV5BMl5BanBnXkFtZTcwNTA4NjIyMQ@@._V1_.jpg",
-					"http://player.youku.com/embed/XMjcyOTMxMg==")
+	# pianist
+	["The Legend of 1900",
+	"https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5NjgyMjEyMV5BMl5BanBnXkFtZTcwNTA4NjIyMQ@@._V1_.jpg",
+	"http://player.youku.com/embed/XMjcyOTMxMg=="],
 
-wolf = media.Movie("Wolf Warrors 2",
-					"An man who tried to save all the people in Africa",
-					"https://upload.wikimedia.org/wikipedia/en/b/b5/Wolf_Warriors_2_poster.jpeg",
-					"http://player.youku.com/embed/XMjQ4Mzc4ODQwOA==")
+	# wolf
+	["Wolf Warrors 2",
+	"https://upload.wikimedia.org/wikipedia/en/b/b5/Wolf_Warriors_2_poster.jpeg",
+	"http://player.youku.com/embed/XMjQ4Mzc4ODQwOA=="],
 
-spider = media.Movie("Spider Man",
-					"The story of our hero - spider man.",
-					"https://upload.wikimedia.org/wikipedia/en/f/f3/Spider-Man2002Poster.jpg",
-					"http://player.youku.com/embed/XMzAxOTkwMTM4MA==")
+	# spiderman
+	["Spider Man",
+	"https://upload.wikimedia.org/wikipedia/en/f/f3/Spider-Man2002Poster.jpg",
+	"http://player.youku.com/embed/XMzAxOTkwMTM4MA=="],
 
-kongfu = media.Movie("Attack & Defence",
-					"Our Papa Ma Yun is playing Kongfu.",
-					"https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=b3f364854190f60310bd9415587bd87e/4d086e061d950a7b3b79a67001d162d9f2d3c932.jpg",
-					"http://player.youku.com/embed/XMzE0ODE3NTgwOA==")
+	# kongfu
+	["Attack & Defence",
+	"https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=b3f364854190f60310bd9415587bd87e/4d086e061d950a7b3b79a67001d162d9f2d3c932.jpg",
+	"http://player.youku.com/embed/XMzE0ODE3NTgwOA=="]
+]
 
+# initialize the movies array
+movies_arr = []
+for movie in movies_data:
+	movie_name = movie[0]
+	movie_imageUrl = movie[1]
+	movie_url = movie[2]
+	movies_arr.append(media.Movie(movie_name, movie_imageUrl, movie_url))
 
-movies = [toy_story, avatar, pianist, wolf, spider, kongfu]
-fresh_tomatoes.open_movies_page(movies)
-# print (media.Movie.VALID_RATINGS)
-# print (media.Movie.__doc__)
-# print (media.Movie.__name__)
-# print (media.Movie.__module__)
+# open movies page in browser
+fresh_tomatoes.open_movies_page(movies_arr)
--- media.py
+++ media.py
@@ -4,9 +4,8 @@ class Movie():
 	""" This class provides a way to store movie related information """
 	VALID_RATINGS = ["G", "PG","PG-13","R"]
 
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youku):
+	def __init__(self, movie_title, poster_image, trailer_youku):
 		self.title = movie_title
-		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_url = trailer_youku
 
